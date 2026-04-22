2b50057a57f02b02d7b46578295cb597fe31d132
ammarayyasy <ammarmojur@gmail.com>
2026-04-22 10:43:45 +0700

    first commit

---
+++ app/Filament/Resources/Offices/Schemas/OfficeForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Offices\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Humaidem\FilamentMapPicker\Fields\OSMMap;
+
+class OfficeForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('latitude')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('longitude')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('radius')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->preload()
+                    ->searchable(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $context): bool => $context === 'create'),
+            ]);
+    }
+}
