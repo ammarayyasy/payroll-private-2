d13f402db323edef4d9091959724d75ab305daca
ammarayyasy <ammarmojur@gmail.com>
2026-04-24 14:23:16 +0700

    Presensi page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Presensi;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/presensi', Presensi::class)->middleware('auth');
