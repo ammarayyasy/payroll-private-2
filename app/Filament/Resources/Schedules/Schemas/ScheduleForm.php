f77d6fed49232a34514794c60ca760189b1c5e70
ammarayyasy <ammarmojur@gmail.com>
2026-04-24 07:55:55 +0700

    redesign UI

--- app/Filament/Resources/Schedules/Schemas/ScheduleForm.php
+++ app/Filament/Resources/Schedules/Schemas/ScheduleForm.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\Schedules\Schemas;
 
 use App\Models\User;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ScheduleForm
@@ -12,15 +15,23 @@ class ScheduleForm
     {
         return $schema
             ->components([
-                Select::make('user_id')
-                    ->options(User::query()->pluck('name', 'id'))
-                    ->searchable(),
-                Select::make('shift_id')
-                    ->relationship('shift', 'name')
-                    ->required(),
-                Select::make('office_id')
-                    ->relationship('office', 'name')
-                    ->required(),
+                Group::make()
+                ->components([
+                    Section::make()
+                    ->components([
+                        Select::make('user_id')
+                            ->options(User::query()->pluck('name', 'id'))
+                            ->searchable(),
+                        Select::make('shift_id')
+                            ->relationship('shift', 'name')
+                            ->required(),
+                        Select::make('office_id')
+                            ->relationship('office', 'name')
+                            ->required(),
+                        Toggle::make('is_wfa')
+                            ->label('WFA'),
+                    ])
+                ])
             ]);
     }
 }
--- app/Filament/Resources/Schedules/Tables/SchedulesTable.php
+++ app/Filament/Resources/Schedules/Tables/SchedulesTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\Schedules\Tables;
 
+use App\Models\Schedule;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\BooleanColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -16,7 +18,10 @@ class SchedulesTable
             ->columns([
                 TextColumn::make('user.name')
                     ->sortable(),
+                BooleanColumn::make('is_wfa')
+                    ->label('WFA'),
                 TextColumn::make('shift.name')
+                    ->description(fn (Schedule $record): string => $record->shift->start_time . ' - ' . $record->shift->end_time)
                     ->sortable(),
                 TextColumn::make('office.name')
                     ->sortable(),
